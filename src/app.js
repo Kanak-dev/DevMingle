6065d4223ccb30c1b9c58f01e6b995fa1fe73b68
Kajal Prajapati <kajal.prajapati@tradeindia.com>
2026-03-03 21:02:59 +0530

    Initial Setup

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express=require('express');
+
+const app=express();
+
+app.use("/hello",(req,res)=>{
+    res.send("Hello Cutie");
+})
+app.use("/api",(req,res)=>{
+    res.send("Hello from the server !!!");
+})
+
+app.listen(3000,()=>{
+    console.log("Server is successfully running on port 3000...")
+});

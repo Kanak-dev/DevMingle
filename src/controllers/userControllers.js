9e76125b1bda9029538ab8ad3ad47f46b2dc0a70
Kajal Prajapati <kajal.prajapati@tradeindia.com>
2026-03-25 16:36:07 +0530

    Created routes

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,6 +1,7 @@
 const User=require("../models/user");
 
 const createUser=async(req,res)=>{
+    //creating new instance of user model 
         const user=new User({
             firstName: req.body.firstName,
             lastName: req.body.lastName,
@@ -17,4 +18,42 @@ const createUser=async(req,res)=>{
 
 }
 
-module.exports={createUser};
+const getUser=async(req,res)=>{
+    const userEmail=req.body.emailId;
+
+    try{
+        const user=await User.find({emailId:userEmail});
+        if(user.length===0){
+            res.status(404).send("User not Found");
+        }else{
+        res.status(200).send(user);
+        }
+    }catch(error){
+        res.status(400).send("Something went Wrong",error);
+    }
+}
+
+const feed=async(req,res)=>{
+
+    try{
+        const user=await User.find({});
+        if(user.length===0){
+            res.status(404).send("No Users");
+        }else{
+        res.status(200).send(user);
+        }
+    }catch(error){
+        res.status(400).send("Something went Wrong",error);
+    }
+}
+
+const deleteUser=async(req,res)=>{
+    const userId=req.body.userId;
+    try{
+        const user=await User.findByIdAndDelete(userId);
+        res.status(204).send("User deleted successfully");
+    }catch(error){
+        res.status(400).send("Something went wrong")
+    }
+}
+module.exports={createUser,getUser,feed,deleteUser};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,5 +3,11 @@ const router=express.Router();
 const userController=require("../controllers/userControllers")
 //routes
 router.post("/signup",userController.createUser);
+router.get("/profile",userController.getUser);
+router.get("/feed",userController.feed);
+router.delete("/delete-user",userController.deleteUser);
+
+
+
 
 module.exports=router;
